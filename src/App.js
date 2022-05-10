2a6e0002b6e7107beffa235f5ab8ec81f1787728
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-05-10 15:11:58 +0600

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div className="App">
-       <Navbar></Navbar>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
