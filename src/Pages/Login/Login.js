2fe545f5148806f7cf68eebbadff0ea401a469d2
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-05-13 02:46:25 +0600

    google sign in done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
+
     return (
-        <div>
-            <h1>please login</h1>
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-xl font-bold">Log In</h2>
+
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline">Google Sign In</button>
+                </div>
+            </div>
         </div>
     );
 };
