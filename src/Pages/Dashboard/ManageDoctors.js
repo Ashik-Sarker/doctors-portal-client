623b4dae1064f624a6b00db98a2d0b996ca1318d
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-05-18 23:01:32 +0600

    manage doctors

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import ManageDoctorsRow from './ManageDoctorsRow';
 
 const ManageDoctors = () => {
     const {
         data: doctors,
-        isLoading
+        isLoading,
+        refetch
     } =
         useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
@@ -20,6 +22,29 @@ const ManageDoctors = () => {
     return (
         <div>
             <h1>Manage Doctors: {doctors.length}</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Specialty</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <ManageDoctorsRow
+                                key={doctor._id}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                            ></ManageDoctorsRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
